3fa092a9896393c6e276973437a638ab2b2531e9
Emmanuel Peralta <manz@users.noreply.github.com>
2023-05-19 14:52:26 +0200

    New release: 1.0.0-alpha.7

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "1.0.0-alpha.6"
+let version = "1.0.0-alpha.7"
 let moduleName = "Woosmap"
-let checksum = "bc9a74e4c5920bd741b5e5241f4cd7c22f610ee3220e8310aae20b62e41f1496"
+let checksum = "a3ee3ed1b503502c78f1791c16f3efb593cbdf39e8dd856415d1bb84fc03a41d"
 let binariesBaseURL = "https://native-sdk.woosmap.com"
 
 let package = Package(
